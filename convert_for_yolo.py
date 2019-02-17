4700ded1ef502827a0ad20d42bea127647685d89
yustina.ivanova <yustina.ivanova@texel.graphics>
2019-02-17 14:10:36 +0300

    yolo training

--- convert_for_yolo.py
+++ convert_for_yolo.py
@@ -7,15 +7,15 @@
 # w, h - width and height for bounding box
 import cv2
 img_name = str(input('input img name: '))
-x, y, w, h, cl = map(int, input('input x, y, w, h, class of the box in the pixels: ').split())
+x_min, y_min, x_max, y_max, cl = map(int, input('input x_min, y_min, x_max, y_max, class of the box in the pixels: ').split())
 # [category number] [object center in X] [object center in Y] [object width in X] [object width in Y]
 img = cv2.imread(img_name + '.JPG')
 file = open(img_name + '.txt', 'w')
 real_height, real_width, _ = img.shape
-xc = round((x + w/2)/real_width, 2)
-yc = round((y + h/2)/real_height, 2)
-wbox = round(w/real_width, 2)
-hbox = round(h/real_height, 2)
+xc = round((x_min + (x_max-x_min)/2)/real_width, 2)
+yc = round((y_min + (y_max-y_min)/2)/real_height, 2)
+wbox = round((x_max-x_min)/real_width, 2)
+hbox = round((y_max-y_min)/real_height, 2)
 file.write(str(cl) + ' ' + str(xc) + ' ' + str(yc) + ' ' + str(wbox) + ' ' + str(hbox))
 file.close()
-print('file was written')
+print(img_name + ' file was written')
